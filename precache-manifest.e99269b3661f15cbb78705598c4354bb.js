7764f4325df5fe3cfbf3101af3184b5c7dc0d4f5
Romain Verbeke <info@kameleon-media.be>
2020-03-24 14:21:36 +0100

    Add files via upload

---
+++ precache-manifest.e99269b3661f15cbb78705598c4354bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb1633be735be5268e8e77002ac791a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a01bf1265c248d0cef22",
+    "url": "/static/css/main.b3def6ec.chunk.css"
+  },
+  {
+    "revision": "bb3e61a641334cb4c755",
+    "url": "/static/js/2.f7b9c512.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7b9c512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a01bf1265c248d0cef22",
+    "url": "/static/js/main.3869086a.chunk.js"
+  },
+  {
+    "revision": "09eb2fae383ea5eae762",
+    "url": "/static/js/runtime-main.074d3413.js"
+  }
+]);
